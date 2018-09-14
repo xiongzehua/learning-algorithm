898eddf4a51839c308ab753a2ff80017b01ee2fa
xiong <313398439@qq.com>
2018-09-14 10:36:19 +0800

    init

---
+++ src/test/java/com/xiongzehua/learning/algorithm/OtherTest.java
@@ -0,0 +1,18 @@
+package java.com.xiongzehua.learning.algorithm;
+
+import org.junit.Test;
+
+/**
+ * 未分类的题目先放在这里
+ * Created by xiongzehua on 2018/9/14.
+ */
+public class OtherTest {
+    /**
+     *
+     */
+    @Test
+    public void leetcode1() {
+
+    }
+
+}
