66288a352bf7dd321d1b779f97f5cf756798c943
xiong <313398439@qq.com>
2018-09-18 23:22:44 +0800

    LeetCode120

---
+++ src/main/java/com/xiongzehua/learning/algorithm/LeetCode120.java
@@ -0,0 +1,46 @@
+package com.xiongzehua.learning.algorithm;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by xiongzehua on 2018/9/18.
+ */
+public class LeetCode120 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        // opt和triangle有相同的结构，并且每个格子保存的是从起点到此格子的最短路径
+        List<List<Integer>> opt = new ArrayList<>();
+        for (int i = 0; i < triangle.size(); i++) {
+            List<Integer> optRow = new ArrayList<>();
+            List<Integer> triangleRow = triangle.get(i);
+
+            if (i == 0) {
+                optRow.add(triangleRow.get(0));
+            } else {
+                for (int j = 0; j < triangleRow.size(); j++) {
+                    int value = 0;
+                    if (j == 0) {
+                        value = opt.get(i - 1).get(j) + triangle.get(i).get(j);
+                    } else if (j == triangleRow.size() - 1) {
+                        value = opt.get(i - 1).get(j - 1) + triangle.get(i).get(j);
+                    } else {
+                        value = Math.min(opt.get(i - 1).get(j - 1), opt.get(i - 1).get(j)) +
+                                + triangle.get(i).get(j);
+                    }
+                    optRow.add(value);
+                }
+            }
+            opt.add(optRow);
+        }
+
+        List<Integer> lastRow = opt.get(opt.size() - 1);
+        lastRow.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return lastRow.get(0);
+    }
+}
--- src/test/java/com/xiongzehua/learning/algorithm/DynamicProgramming.java
+++ src/test/java/com/xiongzehua/learning/algorithm/DynamicProgramming.java
@@ -2,6 +2,10 @@ package com.xiongzehua.learning.algorithm;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -121,6 +125,32 @@ public class DynamicProgramming {
         assertEquals(1, new LeetCode91().numDecodings("27"));
         assertEquals(0, new LeetCode91().numDecodings("01"));
         assertEquals(0, new LeetCode91().numDecodings("303"));
+    }
+
+
+    /**
+     * LeetCode120 Triangle
+     *
+     * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+     * For example, given the following triangle
+     * [
+     *      [2],
+     *     [3,4],
+     *    [6,5,7],
+     *   [4,1,8,3]
+     * ]
+     * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+     * Note:
+     * Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+     */
+    @Test
+    public void leetCode120() {
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Arrays.asList(2));
+        triangle.add(Arrays.asList(3, 4));
+        triangle.add(Arrays.asList(6, 5, 7));
+        triangle.add(Arrays.asList(4, 1, 8, 3));
+        assertEquals(11, new LeetCode120().minimumTotal(triangle));
     }
 
 }
